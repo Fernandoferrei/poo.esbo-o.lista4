123c34949e8133bb39482591764c51f2f57c8ead
Fernandoferrei <fernando.s@aluno.ifce.edu.br>
2024-11-13 09:29:28 -0300

    Add files via upload

---
+++ Fernando.poo/Exe11/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int hora_i, min_i, hora_f, min_f, hora_d, min_d;
+		System.out.println("Digite o horário inicial");
+		hora_i = 10;
+		System.out.println("Minuto: ");
+		min_i = 12;
+		System.out.println("Digite o horário final");
+		System.out.println("horário: ");
+		hora_f = 12;
+		System.out.println("Minuto: ");
+		min_f = 43;
+	}
+
+}
